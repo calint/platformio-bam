0ac67e5fac90a7f14c067a5ad9f53448231b0fd5
calint <calin.tenitchi@gmail.com>
2025-11-15 13:16:36 +0700

    .

--- emu/make.sh
+++ emu/make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-g++ -std=c++23 -O0 -g3 -o bam -Wfatal-errors -Werror -Wall -Wextra -Wpedantic \
+g++ -std=c++17 -O0 -g3 -o bam -Wfatal-errors -Werror -Wall -Wextra -Wpedantic \
     $(pkg-config --cflags --libs sdl3) \
     -Wconversion -Wsign-conversion \
     -Wno-unused-parameter \
@@ -9,3 +9,4 @@ g++ -std=c++23 -O0 -g3 -o bam -Wfatal-errors -Werror -Wall -Wextra -Wpedantic \
     main.cpp
 
 # note: `-flifetime-dse=1` fixes o1store::alloc()...inst->alloc_ptr optimization issue
+# note: matching g++ std to platformio
